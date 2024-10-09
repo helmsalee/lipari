7e72f8a7a4662c5af7ce8d9fcac4c81891a19875
Anthony Angelo <anthonyangelo@yahoo.com>
2024-10-09 17:55:55 -0400

    Update

--- startup.sh
+++ startup.sh
@@ -3,10 +3,5 @@ set -x #echo on
 # General updates
 sudo apt-get update
 # Install basic packages
-sudo apt install jq jwt
-# Install gum
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
-sudo apt update && sudo apt install gum
+sudo apt install jq
 exit 0 # success
